d03c17bafe3f0654d0d535ce96ceca8dad62e090
Baklay Petro <pustota1988@gmail.com>
2022-06-26 10:02:15 +0300

    Finish

---
+++ src/assets/js/mobileNav.js
@@ -0,0 +1,8 @@
+const burger = document.getElementById('sidebarToggle');
+const sidebar = document.getElementById('sidebar');
+
+burger.addEventListener('click', event => {
+
+  document.body.classList.toggle('show-sidebar');
+
+});
